ca451c57acf6ed0bcf60f2ac759e6d0ee95922ef
Aaron <aaroncourt09@gmail.com>
2022-04-11 22:14:31 -0700

    Commit of core assignment Product Manager 2

---
+++ Full Stack Mern/practice-product_manager_2/READEME - Product Manager 1.md
@@ -0,0 +1,10 @@
+# Product Manager 2
+
+The purpose of this assignment was to continue to practice setting up a full-stack React application with modularized files using Mongoose, MongoDB, Express, Axios, and CORS technology. This assignment also allowed us to practice displaying more than one component per page.
+
+This assignment builds on Product Manager 1 by adding a route and component to click through to get the details of a specific item from a list of items displayed on the default page. Here are the details:
+
+>Continue from the previous assignment. On the main page, below the product form, list out all of your products in your database. Then, create a route so that you can see the specifics for a particular product.
+
+Thanks,
+Aaron
---
+++ Full Stack Mern/practice-product_manager_2/client/src/components/ProductList.js
@@ -0,0 +1,34 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import {Link} from 'react-router-dom';
+
+const ProductList = (props) => {
+    const {products, setProducts} = props;
+
+    useEffect( () => {
+        axios.get('http://localhost:8000/api/products')
+            .then(res => {
+                console.log(res.data)
+                setProducts(res.data);
+            })
+            .catch(err => {
+                console.log(err)
+            });
+    }, []);
+
+    return(
+        <div>
+            {
+                products.map( (product, index) => {
+                    return(
+                        <div key={index}>
+                            <Link to={`/api/products/${product._id}`}>{product.title} detail page</Link>
+                        </div>
+                    );
+                })
+            }
+        </div>
+    );
+};
+
+export default ProductList
