5e79f6eeb880a6e35cda0ac1569218081a7f74ca
Aaron <aaroncourt09@gmail.com>
2022-04-14 14:39:19 -0700

    corrected display error on newly created items

--- Full Stack Mern/core-product_manager_2/server/controllers/product.controller.js
+++ Full Stack Mern/core-product_manager_2/server/controllers/product.controller.js
@@ -5,7 +5,7 @@ module.exports = {
     createProduct: (req, res) => {
         Product.create(req.body)
             .then(newProduct => 
-                res.json({product: newProduct}))
+                res.json(newProduct))
             .catch(err => {
                 res.json({message: 'Something went wrong: ', error: err})
             })
