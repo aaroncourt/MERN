2ea978faac96b14c79e223909b2127cf16b76e19
Aaron <aaroncourt09@gmail.com>
2022-04-21 14:03:31 -0700

    commit of practice assignment Ninja

---
+++ OOP and Class Components/practice-ninja/practice-ninja/ninja.js
@@ -0,0 +1,8 @@
+class Ninja {
+    constructor(ninjaName, ninjaHealth) {
+        this.ninjaName = ninjaName;
+        this.ninjaHealth = ninjaHealth;
+        this.ninjaSpeed = 3;
+        this.ninjaStrength = 3;
+    }
+}
